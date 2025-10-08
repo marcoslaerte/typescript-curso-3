d48e719b9240b87105bdc113abdbf820ae54a680
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-10-08 16:41:51 -0300

    Criando a interface Imprimível para evitar erros de objetos inválivos

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,4 +1,6 @@
-export class Negociacao {
+import { Imprimivel } from "../utils/imprimivel.js";
+
+export class Negociacao implements Imprimivel {
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
@@ -29,4 +31,11 @@ export class Negociacao {
             Valor: ${this.valor}   
         `;
     }
+
+    public ehIgual(negociacao: Negociacao): boolean {
+        return this.data.getDate() === negociacao.data.getDate()
+            && this.data.getMonth() === negociacao.data.getMonth()
+            && this.data.getFullYear() === negociacao.data.getFullYear()
+    }
+
 }
